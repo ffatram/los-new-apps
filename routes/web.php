2fb96170d776f7f80debb17ff9dabf1fca56b687
ffatram <faturungimuharram@gmail.com>
2025-07-02 14:57:15 +0800

    test ubah route
    testing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('pages.dashboard');
+// });
+
 Route::get('/', function () {
-    return view('pages.dashboard');
+    return view('pages.home');
 });
