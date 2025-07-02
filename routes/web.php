4a04f55a1e87ae144667945f6f2383583132e513
ffatram <faturungimuharram@gmail.com>
2025-07-02 14:58:59 +0800

    test 2

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('pages.dashboard');
-// });
 
 Route::get('/', function () {
     return view('pages.home');
+});
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
 });
